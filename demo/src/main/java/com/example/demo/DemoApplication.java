04a787e2ab889ebc8c595342fd55aa87c397b981
yuwenjie <yuwenjie@cetc-cloud.com>
2018-12-03 16:30:49 +0800

    SpringBoot学习-HelloWorld  打包

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+//用来标注这是一个Spring Boot 应用
 @SpringBootApplication
+@RestController //标注这个程序还是一个控制器
 public class DemoApplication {
 
+    @RequestMapping("/")
+    String home()
+    {
+        return "hello";
+    }
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 }
+
+//https://github.com/chenfromsz/spring-boot-hello.git
+
